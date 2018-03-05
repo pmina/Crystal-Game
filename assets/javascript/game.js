8092217d9309bd0678c0039b967664b0f43a8acd
pmina <philomn9@gmail.com>
2018-03-05 14:34:56 -0500

    m

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,83 +1,82 @@
 
 
-
-var random = Math.floor(Math.random() * 101 + 19);
-console.log(random)
-$("#randomNumber").text(random);
-
-
-var num1 = Math.floor(Math.random()*11+1);
-var num2 = Math.floor(Math.random()*11+1);
-var num3 = Math.floor(Math.random()*11+1);
-var num4 = Math.floor(Math.random()*11+1);
+        var random = Math.floor(Math.random() * 101 + 19);
+            console.log(random)
+            $("#randomNumber").text(random);
 
 
-var userTotal = 0;
-var wins = 0;
-var losses = 0;
-
-$("#numberWins").text(wins);
-$("#numberLosses").text(losses);
+        var num1 = Math.floor(Math.random()*11+1);
+        var num2 = Math.floor(Math.random()*11+1);
+        var num3 = Math.floor(Math.random()*11+1);
+        var num4 = Math.floor(Math.random()*11+1);
 
 
-var yayy = function(){
-alert("Congrats, you won!");
-wins++;
-$("#numberWins").text(wins);
-;
-}
+        var userTotal = 0;
+        var wins = 0;
+        var losses = 0;
 
-var loser = function(){
-alert("You lose!");
-losses++;
-$("#numberLosses").text(losses);
+        $("#numberWins").text(wins);
+        $("#numberLosses").text(losses);
 
-}
 
-$('#one').on ('click', function(){
-userTotal = userTotal + num1;
-console.log("New userTotal= " + userTotal);
-$('#finalTotal').text(userTotal); 
-    if (userTotal == random){
-    yayy();
-    }
-    else if ( userTotal > random){
-    loser();
-    }   
-})
+        var yayy = function(){
+            alert("Congrats, you won!");
+            wins++;
+            $("#numberWins").text(wins);
+            ;
+        }
 
-$('#two').on ('click', function(){
-userTotal = userTotal + num2;
-console.log("New userTotal= " + userTotal);
-$('#finalTotal').text(userTotal); 
-    if (userTotal == random){
-    yayy();
-    }
-    else if ( userTotal > random){
-    loser();
-    }   
-})
+        var loser = function(){
+            alert("You lose!");
+            losses++;
+            $("#numberLosses").text(losses);
+            
+        }
 
-$('#three').on ('click', function(){
-userTotal = userTotal + num3;
-console.log("New userTotal= " + userTotal);
-$('#finalTotal').text(userTotal); 
-    if (userTotal == random){
-    yayy();
-    }
-    else if ( userTotal > random){
-    loser();
-    }   
-})
+        $('#one').on ('click', function(){
+            userTotal = userTotal + num1;
+            console.log("New userTotal= " + userTotal);
+            $('#finalTotal').text(userTotal); 
+                if (userTotal == random){
+                yayy();
+                }
+                else if ( userTotal > random){
+                loser();
+                }   
+        })
 
-$('#four').on ('click', function(){
-userTotal = userTotal + num4;
-console.log("New userTotal= " + userTotal);
-$('#finalTotal').text(userTotal); 
-    if (userTotal == random){
-    yayy();
-    }
-    else if ( userTotal > random){
-    loser();
-    }   
-})
+        $('#two').on ('click', function(){
+            userTotal = userTotal + num2;
+            console.log("New userTotal= " + userTotal);
+            $('#finalTotal').text(userTotal); 
+                if (userTotal == random){
+                yayy();
+                }
+                else if ( userTotal > random){
+                loser();
+                }   
+        })
+
+        $('#three').on ('click', function(){
+            userTotal = userTotal + num3;
+            console.log("New userTotal= " + userTotal);
+            $('#finalTotal').text(userTotal); 
+                if (userTotal == random){
+                yayy();
+                }
+                else if ( userTotal > random){
+                loser();
+                }   
+        })
+
+        $('#four').on ('click', function(){
+            userTotal = userTotal + num4;
+            console.log("New userTotal= " + userTotal);
+            $('#finalTotal').text(userTotal); 
+                if (userTotal == random){
+                yayy();
+                }
+                else if ( userTotal > random){
+                loser();
+                }   
+        })
